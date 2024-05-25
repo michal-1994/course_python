116ccbf9c5f02abc99c355b9ff0452887f0827b5
Michal <michalgrzegorczyk04@gmail.com>
2024-05-25 11:17:32 +0200

    add other method to quit

--- blockchain.py
+++ blockchain.py
@@ -56,7 +56,9 @@ def veryfy_chain():
   return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
   print('Please choose')
   print('1: Add a new transaction value')
   print('2: Output the blockchain blocks')
@@ -72,7 +74,7 @@ while True:
     if len(blockchain) >= 1:
       blockchain[0] = [2]
   elif user_choice == 'q':
-    break
+    waiting_for_input = False
   else:
     print('Input was invalid, please pick a value from the list!')
   if not veryfy_chain():
