952115d4b60f39bd0add95e2a32f8eff31b2df7a
Michal <michalgrzegorczyk04@gmail.com>
2024-05-25 16:48:28 +0200

    add doc to get balance method

--- blockchain.py
+++ blockchain.py
@@ -19,10 +19,21 @@ def hash_block(block):
 
 
 def get_balance(participant):
+  """Calculate and return the balance for a participant.
+
+  Arguments:
+    :participant: The person for whom to calculate the balance.
+  """
+  # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
+  # This fetches sent amounts of transactions that were already included in blocks of the blockchain
   tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+  # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
+  # This fetches sent amounts of open transactions (to avoid double spending)
   open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
   tx_sender.append(open_tx_sender)
   amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+  # This fetches received coin amounts of transactions that were already included in blocks of the blockchain
+  # We ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed + included in a block
   tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
   amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
   # Return the total balance
