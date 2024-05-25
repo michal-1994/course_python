81af5baf23a2d2860f031cdd73a72c73947795dc
Michal <michalgrzegorczyk04@gmail.com>
2024-05-25 12:51:57 +0200

    hash previous block

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,7 @@ genesis_block = {
   'index': 0,
   'transactions': []
 }
-blockchain = []
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Max'
 
@@ -34,8 +34,17 @@ def add_transaction(recipient, sender = owner, amount = 1.0):
 
 def mine_block():
   last_block = blockchain[-1]
+  hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+
+  print(hashed_block)
+
+  for keys in last_block:
+    value = last_block[keys]
+    hashed_block = hashed_block + str(value)
+
+  print(hashed_block)
   block = {
-    'previous_hash': 'XYZ',
+    'previous_hash': hashed_block,
     'index': len(blockchain),
     'transactions': open_transactions
   }
@@ -80,7 +89,8 @@ waiting_for_input = True
 while waiting_for_input:
   print('Please choose')
   print('1: Add a new transaction value')
-  print('2: Output the blockchain blocks')
+  print('2: Mine a new block')
+  print('3: Output the blockchain blocks')
   print('h: Manipulate the chain')
   print('q: Quit')
   user_choice = get_user_choice()
@@ -90,6 +100,8 @@ while waiting_for_input:
     add_transaction(recipient, amount = amount)
     print(open_transactions)
   elif user_choice == '2':
+    mine_block()
+  elif user_choice == '3':
     print_blockchain_elements()
   elif user_choice == 'h':
     if len(blockchain) >= 1:
@@ -98,10 +110,10 @@ while waiting_for_input:
     waiting_for_input = False
   else:
     print('Input was invalid, please pick a value from the list!')
-  if not veryfy_chain():
-    print('Invalid blockchain!')
-    print_blockchain_elements()
-    break
+  # if not veryfy_chain():
+  #   print('Invalid blockchain!')
+  #   print_blockchain_elements()
+  #   break
 else:
   print('User left!')
 
